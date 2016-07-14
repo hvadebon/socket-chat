098656971c02f9a215b5a5bc3760f657eb426f55
hvadebon <h.vadebon@gmail.com>
2016-07-13 20:34:54 -0600

    fourth

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('youchatwithme.herokuapp.com
+app.get('git.heroku.com/youchatwithme.git
 ', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
