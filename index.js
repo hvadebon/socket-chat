d1fc01e17b4a200df74917e3dae5cd98de8e6db8
hvadebon <h.vadebon@gmail.com>
2016-07-13 20:20:45 -0600

    second try

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('https://git.heroku.com/youchatwithme.git
+', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
